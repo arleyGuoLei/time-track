001b3fddc703e635331bd8418cae5fb37c9b437e
arleyGuoLei <34805850@qq.com>
2020-12-27 00:09:43 +0800

    引入数据查询组件

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
+import uniClientdb from '@/components/uni-clientdb.vue'
 
 Vue.config.productionTip = false
+Vue.component('unicloud-db', uniClientdb)
 new App().$mount()
