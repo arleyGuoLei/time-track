4dd01fc37a76628feb0bbe06d3a93778b6a6d9ae
arleyGuoLei <34805850@qq.com>
2021-01-02 09:50:41 +0800

    增加router

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
 import uniClientdb from '@/components/uni-clientdb.vue'
+import { router, RouterMount } from '@/utils/router'
+
+Vue.use(router)
 
 Vue.config.productionTip = false
 Vue.component('unicloud-db', uniClientdb)
-new App().$mount()
+
+const app = new Vue({
+  ...App,
+})
+
+// #ifdef H5
+RouterMount(app, router, '#app')
+// #endif
+
+// #ifndef H5
+app.$mount() //为了兼容小程序及app端必须这样写才有效果
+// #endif
