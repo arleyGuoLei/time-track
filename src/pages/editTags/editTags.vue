e366ceb836cf814f87eab91cd30bc67fb72d893c
jessie <1185922395@qq.com>
2021-01-05 01:04:29 +0800

    修复字节小程序样式

--- src/pages/editTags/editTags.vue
+++ src/pages/editTags/editTags.vue
@@ -4,26 +4,26 @@
 
     <view class="tag-item-container" v-for="(item, index) in tagsList" :key="index">
       <view class="edit-content flex" v-if="showEdit && editId === item._id">
-        <input type="text" placeholder="请输入标签" focus v-model="item.name" />
-        <view class="action-btn text-sm bg-theme" @click="onUpdate(item)">确认修改</view>
-        <view class="action-btn text-sm bg-red" @click="showEdit = false">取消</view>
+        <input class="text input" type="text" placeholder="请输入标签" focus v-model="item.name" />
+        <view class="action-btn text-sm bg-theme text-sm margin-right" @click="onUpdate(item)">确认修改</view>
+        <view class="action-btn text-sm bg-red text-sm" @click="showEdit = false">取消</view>
       </view>
       <view class="tag-content flex" v-else>
-        <view>
-          <text class="margin-right">{{ item.name }}</text>
+        <view class="flex">
+          <text class="margin-right text">{{ item.name }}</text>
           <text class="text-grey text-sm">含{{ item.eventNumber }}项</text>
         </view>
         <view class="handle-container">
-          <text class="margin-right text-red" @click="onDelete(item._id, item.name, index)">删除</text>
-          <text class="margin-right text-grey" @click="onEdit(item._id)">编辑</text>
+          <text class="margin-right text-red text-sm" @click="onDelete(item._id, item.name, index)">删除</text>
+          <text class="margin-right text-grey text-sm" @click="onEdit(item._id)">编辑</text>
         </view>
         <img class="icon-move-up" src="@/static/edit-tags-move-up.png" @click="onMove(index, item)" />
       </view>
     </view>
 
     <view class="tag-item-container flex">
-      <input type="text" placeholder="请输入标签" v-model="tagValue" />
-      <view class="action-btn text-sm bg-theme" @click="onSave">保存</view>
+      <input class="text" type="text" placeholder="请输入标签" v-model="tagValue" />
+      <view class="action-btn text-sm bg-theme text-sm" @click="onSave">保存</view>
     </view>
   </view>
 </template>
