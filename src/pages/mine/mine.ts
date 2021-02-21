9880465f7cd5afbe10260baf86e403532cc3568e
jessie <1185922395@qq.com>
2021-02-21 21:21:31 +0800

    完成个人中心样式,数据填充

--- src/pages/mine/mine.ts
+++ src/pages/mine/mine.ts
@@ -1,8 +1,33 @@
 import { Vue, Component } from 'vue-property-decorator'
 
+declare module 'vue/types/vue' {
+  interface Vue {
+    CustomBar: number
+    StatusBar: number
+  }
+}
 @Component
 export default class extends Vue {
+  private list = [
+    {
+      iconName: 'share',
+      title: '分享给朋友',
+      openType: 'share',
+    },
+    {
+      iconName: 'advice',
+      title: '意见反馈',
+      openType: 'feedback',
+    },
+    {
+      iconName: 'about',
+      title: '关于我们',
+    },
+  ]
   onLoad() {
-    console.log('mine')
+    ;(this as any).$loading('getInfo', this.getInfo.bind(this))
+  }
+  getInfo() {
+    console.log('getUserInfo')
   }
 }
--- src/pages/mine/mine.vue
+++ src/pages/mine/mine.vue
@@ -1,5 +1,31 @@
 <template>
-  <view class="page"> </view>
+  <view class="page">
+    <view class="user-container">
+      <view class="avator">
+        <img src="http://img.i7xy.cn/20201208234148.png" />
+      </view>
+      <view class="name mt-28 text-white">小木子</view>
+      <view class="info-container text-white">
+        <view class="dot-num">
+          <view class="num text-large text-bold">321</view>
+          <view class="desc text-xs">打点次数</view>
+        </view>
+        <view class="line bg-white"></view>
+        <view class="day-num">
+          <view class="num text-large text-bold">123</view>
+          <view class="desc text-xs">打点天数</view>
+        </view>
+      </view>
+    </view>
+    <view class="list-container bg-white">
+      <button class="list flex margin-bottom-sm" v-for="(item, index) in list" :key="index" :open-type="item.openType">
+        <view class="left flex">
+          <img class="list-icon mr-24" :src="`../../static/mine-${item.iconName}.png`" />
+          <span>{{ item.title }}</span>
+        </view>
+      </button>
+    </view>
+  </view>
 </template>
 
 <script lang="ts" src="./mine.ts"></script>
