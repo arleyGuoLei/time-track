d028fed1c4417e387207e4e692bfaab7321ee742
arleyGuoLei <34805850@qq.com>
2021-11-06 12:50:37 +0800

    feat: 增加项目文档

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -4,18 +4,12 @@
 export const CLOUD_ENV = {
   dev: {
     provider: 'aliyun',
-    spaceId: 'b1cf53e8-922d-4868-8598-5111a35fcab5',
-    clientSecret: 'xZWd3HjxRYTLec0eEk7njg==',
+    spaceId: '',
+    clientSecret: '',
   },
-  // mock
-  // prod: {
-  //   provider: 'aliyun',
-  //   spaceId: 'b1cf53e8-922d-4868-8598-5111a35fcab5',
-  //   clientSecret: 'xZWd3HjxRYTLec0eEk7njg==',
-  // },
   prod: {
     provider: 'aliyun',
-    spaceId: '5988d11f-2178-4757-82ed-80342d2c8132',
-    clientSecret: 'hVAvAxYdBNbz2XHWTvDGqQ==',
+    spaceId: '',
+    clientSecret: '',
   },
 }
